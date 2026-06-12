8e9ee58f0810803b47b701f4de6420be98ad36e6
ZayanAhmadGhous <zayanahmd851@gmail.com>
2026-06-12 06:09:36 +0500

    Adjust Formatting of main file

--- app/main.py
+++ app/main.py
@@ -24,7 +24,9 @@ class Data(BaseModel):
 
 def get_cache():
     return redis.Redis(
-        host=os.getenv("REDIS_HOST", "localhost"), port=6379, decode_responses=True
+        host=os.getenv("REDIS_HOST", "localhost"),
+        port=6379,
+        decode_responses=True
     )
 
 
